1ac7b70648ec129030585c21b73ede319e3ea88b
Omkar Gavade <omkarvgawade@gmai.com>
2023-06-17 03:01:58 +0530

    added dummy login credentials for hussle free login

--- valet-app/src/Components/Login/Login.js
+++ valet-app/src/Components/Login/Login.js
@@ -9,20 +9,32 @@ import {
 import { useSelector, shallowEqual, useDispatch } from "react-redux";
 import { useState } from "react";
 import { doLogin } from "../../Redux/Auth/AuthAction";
-import PasswordInput from "../PasswordInput";
 import { checkForEmptyInputs } from "../../Utils/HelperFunction";
 import { textFieldStyle } from "../../Styles/MUI/Mui";
 import { useNavigate } from "react-router-dom";
 import { getAuthTokenStatus } from "../../Routes/AppRoutes";
 import {Link} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useRef } from "react";
 function Login() {
   const { auth } = useSelector(({ auth }) => auth, shallowEqual) || {};
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const toShowInfo = useRef(true);
 
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   const [loginDetails, setLoginDetails] = useState({
-    email: "",
-    password: "",
+    email: "pestoproject@gmail.com",
+    password: "123456",
   });
 
   const handleLoginFormChange = (event) => {
@@ -48,6 +60,22 @@ function Login() {
       dispatch(doLogin(loginDetails));
     }
   };
+  useEffect(()=>{
+    if(toShowInfo.current){
+      toShowInfo.current = false;
+      toast.info('Please Note, We have given dummy login credentials for hussle free login. ', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    }
+
+  },[])
   return (
     <div className="user-auth-login-cont">
       <div className="login-text">
@@ -57,6 +85,7 @@ function Login() {
         <div>
           <TextField
             name="email"
+            value={loginDetails.email}
             placeholder="valet@example.com"
             variant="standard"
             label="Email"
@@ -67,7 +96,33 @@ function Login() {
           />
         </div>
         <div>
-          <PasswordInput onChange={handleLoginFormChange} />
+        <TextField
+        name="password"
+        sx={textFieldStyle}
+        placeholder="abc@123"
+        label={"Password"}
+        value={loginDetails.password}
+        type={showPassword ?"text":"password"}
+        style={{paddingLeft:"-4rem"}}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+        variant="standard"
+        size="medium"
+        fullWidth
+        onChange={handleLoginFormChange}
+      />
           <div>
             <Link to="/forgotpassword">Forgot Password?</Link>
           </div>
